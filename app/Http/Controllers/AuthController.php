a2afe38de1f2a53bb0aeceb0ac3705039300bf26
Nabil-Kaindra <nabilkaindrabudiantana@gmail.com>
2024-11-01 14:00:15 +0700

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function showLoginForm() {
+        return view('auth.login');
+    }
+
+    public function postLogin(Request $request) {
+        $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+        
+        $credentials = $request->only('username', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('home')->with('success', 'Anda berhasil login.');
+        }
+
+        return back()->withErrors([
+            'username' => 'Username atau password salah.',
+        ]);
+    }
+
+    public function showRegistrationForm() {
+        return view('auth.register');
+    }
+
+    public function register(Request $request) {
+        $request->validate([
+            'username' => 'required|string|unique:users,username|max:255', // Memperbaiki validasi
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+        
+        User::create([
+            'username' => $request->username,
+            'password' => Hash::make($request->password), 
+        ]);
+        
+        return redirect()->route('login')->with('success', 'Anda berhasil mendaftar, silakan login.');
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+        
+        return redirect()->route('login')->with('success', 'Anda berhasil logout.');
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    protected $primaryKey = 'userID';
+
+
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        'namalengkap',
+        'alamat'
+    ];
+
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
