1f05f9e8249f17e2ed5298419a9c5d1b0e4ad284
Nabil-Kaindra <nabilkaindrabudiantana@gmail.com>
2024-11-13 10:38:46 +0700

    adjusting home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <a href="{{ route('albums.photos', $album->albumID) }}" class="text-decoration-none text-dark">
                 <div class="card fixed-card h-100">
-                    {{-- Tampilkan foto pertama dari album --}}
+                    <!-- Tampilkan foto pertama dari album -->
                     @if($album->photos->isNotEmpty())
                         <img src="{{ asset('storage/' . $album->photos->first()->lokasiFile) }}" 
                             alt="{{ $album->judulAlbum }}" 
@@ -20,8 +20,8 @@
                             class="card-img-top fixed-image">
                     @endif
                     <div class="card-body text-center">
-                        <h5 class="card-title">{{ $album->judulAlbum }}</h5>
-                        <p class="card-text">{{ Str::limit($album->deskripsiAlbum, 100) }}</p>
+                        <h5 class="card-title">{{ $album->namaAlbum }}</h5>
+                        <p class="card-text">{{ Str::limit($album->deskripsi, 100) }}</p>
                         <p class="card-text">
                             <small class="text-muted">{{ $album->created_at->format('d M Y') }}</small>
                         </p>
